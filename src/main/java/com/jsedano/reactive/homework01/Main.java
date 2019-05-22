1c2f95d8c464d2578fed0756cdfa217aab55a2d8
jsedano <jsedano@kenzan.com>
2019-05-21 23:28:49 -0500

    changes

--- src/main/java/com/jsedano/reactive/homework01/Main.java
+++ src/main/java/com/jsedano/reactive/homework01/Main.java
@@ -1,9 +1,7 @@
 package com.jsedano.reactive.homework01;
 
-import java.io.File;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.List;
-
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,21 +10,22 @@ import io.reactivex.Observable;
 
 public class Main {
 
-    
-    
+
+
     public static void main(String []args) throws Exception {
-        
+
+        InputStream is = Main.class.getResourceAsStream("/news.json");
+
         NewsItemSubscribersCollection subscribersCollection = new NewsItemSubscribersCollection();
-       
-        File f = Paths.get(Main.class.getClassLoader().getResource("news.json").toURI()).toFile();
-        
-        
-        List<NewsItem> li = new ObjectMapper().readValue(f, new TypeReference<List<NewsItem>>(){});
-        
-        
+
+
+
+        List<NewsItem> li = new ObjectMapper().readValue(is, new TypeReference<List<NewsItem>>(){});
+
+
         Observable<NewsItem> observable = Observable.fromIterable(li);
-        
-        
+
+
         observable.safeSubscribe(subscribersCollection.getSportsObserver());
         observable.safeSubscribe(subscribersCollection.getShowBusinessObserver());
         observable.safeSubscribe(subscribersCollection.getShowBusinessAndSportObserver());
